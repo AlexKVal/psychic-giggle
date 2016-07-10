ffab8e3466ed79e09fa84e3e04d42b7e7de463d7
AlexKVal <alexkval@gmail.com>
2016-07-10 23:39:12 +0300

    initial

---
+++ web/router.ex
@@ -0,0 +1,26 @@
+defmodule VideoStream.Router do
+  use VideoStream.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", VideoStream do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", VideoStream do
+  #   pipe_through :api
+  # end
+end
